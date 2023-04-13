840acd8d4127f56c4542049a1b8c01cf9a682c1f
Andrew Littlejohn <andrewlittlejohn@andrews-air.myfiosgateway.com>
2023-04-13 15:29:06 -0400

    here we go

---
+++ README.md
@@ -0,0 +1,15 @@
+#Challenge No. 19 - PWA Text Editor
+
+
+I'm really like learning the steps of fully deploying things, still don't love Heroku but since we didn't have to work with SQL I was better off!
+
+## Things to learn
+
+1. I haven't gotten the install button to work, I will continue to work on this but at this point I'm at a loss, I've used code that multiple times doesn't give me an error but I can't get it open and pop out like it is supposed to do. 
+
+## Lessons Learned
+
+1. I'm finally getting a bit of the hang of Heroku
+
+2. I know there is a way to get your gitHub and Heroku setup so that everytime a change is made I don't have
+to do the delete dist, npm run build, and then get everything back to git and Heroku. I wish I had looked into this, would have saved some time.
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,30 +2,22 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-// from 19-PWA/01-Activities/25-Ins_Manifest/assets/js/install.js
 window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault();
-  window.deferredPrompt = event;
-  butInstall.style.visibility = 'visible';
-  });
+  window.deferredInstallPrompt = event;
+  butInstall.classList.remove('hidden');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  if (window.beforeinstallprompt) {
-    const promptEvent = window.beforeinstallprompt;
-      promptEvent.prompt();
-    
-    const choice = await promptEvent.userChoice;
-      if (choice.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('Install Promt Dismissed');
-    };
-  }
+  if (!window.deferredInstallPrompt) {
+    return;
+  } 
+  await window.deferredInstallPrompt.prompt();
+  window.deferredInstallPrompt = null;
+  butInstall.classList.add('hidden');
 });
 
 // TODO: Add an handler for the `appinstalled` event
-// from 19-PWA/01-Activities/25-Ins_Manifest/assets/js/install.js
-window.addEventListener('appinstalled', (event) => {
-  console.log('👍', 'appinstalled', event);
+window.addEventListener('appinstalled', () => {
+  window.deferredInstallPrompt = null;
 });
