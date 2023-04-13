81b613be033294fd49b46134a553a0006289c221
Andrew Littlejohn <andrewlittlejohn@andrews-air.myfiosgateway.com>
2023-04-13 10:55:19 -0400

    making progress

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,17 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['document','image', 'script', 'style', 'font'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'PWA-TextEditor-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60, 
+      }),
+    ],
+  })
+);
